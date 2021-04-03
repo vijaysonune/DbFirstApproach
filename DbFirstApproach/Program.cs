c4537f6a7a88ca2b72db64cba71b30f646b2744b
vsonune <vsonune@EC2AMAZ-S4GP8VO>
2021-04-03 20:32:41 +0530

    Add project files.

---
+++ DbFirstApproach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoDbContext();
+
+            var result = context.GetCourses();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Title);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
